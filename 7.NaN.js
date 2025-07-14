997d99c8acc3015c184efbd2d70222100e4e56c2
ydv-kanchan <ydv.kanchan24@gmail.com>
2025-07-14 15:22:12 +0530

    NaN data type

---
+++ 7.NaN.js
@@ -0,0 +1,6 @@
+// NaN stands for "Not-a-Number". It’s a special numeric value that represents the
+//  result of a mathematically invalid number operation.
+let result = 0 / 0;
+console.log(result); // NaN
+
+console.log(typeof NaN); // "number"
