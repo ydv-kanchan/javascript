e65eec63d56b96aa49d19f44e62b6d0cd1759011
ydv-kanchan <ydv.kanchan24@gmail.com>
2025-07-14 09:53:51 +0530

    hello world, how to quote strings, comments

--- 1.helloworld.js
+++ 1.helloworld.js
@@ -1,6 +1,6 @@
 console.log("hello world !!");
 
-console.log(`hello world 2`)
+console.log(`hello world 2`);
 
-// you can use double quotes, single quotes and backticks for quoting the strings in js
+// you can use double quotes (" "), single quotes (' ') and backticks (` `)for quoting the strings in js
 // simi colon in the end of each statement of block is optional, but it's good  practice to use it
